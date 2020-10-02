2171b744f85f527aed8cbf14751383f4fbeaef41
Sam Wray <sam@wray.pro>
2020-10-02 16:00:13 +0100

    build: adds eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "@vue/prettier"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prefer-const": [
+      "error",
+      {
+        destructuring: "any",
+        ignoreReadBeforeAssign: false
+      }
+    ],
+    curly: "error"
+  },
+  parserOptions: {
+    parser: "babel-eslint"
+  }
+};
