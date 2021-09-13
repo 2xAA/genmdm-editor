2cfad07fc537692d97df2995cc300a667b0907b3
Sam Wray <sam@wray.pro>
2021-09-13 20:26:47 +0100

    feat(electron): bootstraps electron
    fix #72

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,25 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/genmdm-editor/" : "/",
 
+  configureWebpack: {
+    devtool: "source-map"
+  },
+
   pluginOptions: {
     svgLoader: {
       svgo: {
         plugins: []
+      }
+    },
+
+    electronBuilder: {
+      builderOptions: {
+        // options placed here will be merged with default configuration and passed to electron-builder
+        appId: "fm.2xaa.genmdmeditor",
+
+        linux: {
+          category: "Midi"
+        }
       }
     }
   }
