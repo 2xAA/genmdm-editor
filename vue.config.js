65a9ac233c39d59535572c6b79bbee5cddc7e90d
Sam Wray <sam@wray.pro>
2021-09-13 21:24:58 +0100

    fix(ci): fixes release with electron-builder and semantic release (#76)

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+const publishingOptions = {
+  provider: "github",
+  releaseType: "prerelease",
+  vPrefixedTagName: false
+};
+
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/genmdm-editor/" : "/",
 
@@ -19,7 +25,29 @@ module.exports = {
 
         linux: {
           category: "Midi"
-        }
+        },
+
+        snap: {
+          confinement: "classic",
+          publish: publishingOptions
+        },
+
+        // See https://www.electron.build/configuration/mac
+        mac: {
+          // See https://developer.apple.com/documentation/security/hardened_runtime
+          hardenedRuntime: true,
+          // See https://github.com/vcync/modV/issues/413
+          strictVerify: false,
+          gatekeeperAssess: false
+        },
+
+        dmg: {
+          sign: false
+        },
+
+        afterSign: "build/notarize.js",
+
+        publish: publishingOptions
       }
     }
   }
